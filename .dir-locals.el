e0cd490b7c531bd119b7965fe2bc30882312e2b2
funnydog <funnydog@users.noreply.github.com>
2023-08-23 20:14:50 +0200

    Add the freetype2 path into the .dir-local.el variables.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((c++-mode . ((flycheck-clang-language-standard . "c++17"))))
+((c++-mode . ((flycheck-clang-include-path . ("/usr/include/freetype2"))
+	      (flycheck-clang-language-standard . "c++17"))))
