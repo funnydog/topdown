69ceb4034ffef5554e391bcba012a7df56f89dd3
funnydog <funnydog@users.noreply.github.com>
2023-08-23 16:27:37 +0200

    Add some Texture and Font resources and their holders.

--- resources.hpp
+++ resources.hpp
@@ -9,3 +9,23 @@ enum class StateID
 	GamePlay,
 	GameOver,
 };
+
+enum class TextureID
+{
+	Explosion,
+};
+
+enum class FontID
+{
+	Title,
+	Body,
+};
+
+template <typename Resource, typename Identifier>
+class ResourceHolder;
+
+class Texture;
+typedef ResourceHolder<Texture, TextureID> TextureHolder;
+
+class Font;
+typedef ResourceHolder<Font, FontID> FontHolder;
