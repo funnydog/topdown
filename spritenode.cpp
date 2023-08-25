e87b6fa228af2080e2c48935a35216b1052db6c3
funnydog <funnydog@users.noreply.github.com>
2023-08-25 06:41:45 +0200

    Use the world transform to draw the sprite.

--- spritenode.cpp
+++ spritenode.cpp
@@ -18,5 +18,5 @@ SpriteNode::updateCurrent(Time)
 void
 SpriteNode::drawCurrent(RenderTarget &target) const
 {
-	mSprite.draw(target, glm::mat4(1.f));//getWorldTransform());
+	mSprite.draw(target, getWorldTransform());
 }
