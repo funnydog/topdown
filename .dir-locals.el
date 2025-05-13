e849a4c28c1208a390fc78885e608f1ab531811f
funnydog <funnydog@users.noreply.github.com>
2025-05-13 05:49:18 +0200

    Compile the project with the C++20 standard.

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,4 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((c++-mode . ((flycheck-clang-include-path . ("/usr/include/freetype2"))
-	      (flycheck-clang-language-standard . "c++17"))))
+	      (flycheck-clang-language-standard . "c++20"))))
