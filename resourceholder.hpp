8a7deb6b4213e3e59f7a5924f0e103446be0de45
funnydog <funnydog@users.noreply.github.com>
2025-05-13 05:54:51 +0200

    Add ResourceHolder::destroy() to destroy each resource.

--- resourceholder.hpp
+++ resourceholder.hpp
@@ -21,6 +21,8 @@ public:
 	Resource& get(Identifier id);
 	const Resource& get(Identifier id) const;
 
+	void destroy();
+
 private:
 	std::unordered_map<Identifier, ResourcePtr> mResourceMap;
 };
@@ -71,4 +73,14 @@ ResourceHolder<Resource, Identifier>::get(Identifier id) const
 	assert(found != mResourceMap.end() && "Resource not found");
 
 	return *found->second;
+}
+
+template <typename Resource, typename Identifier>
+void
+ResourceHolder<Resource, Identifier>::destroy()
+{
+	for (auto &[_, ptr] : mResourceMap)
+	{
+		ptr->destroy();
+	}
 }
